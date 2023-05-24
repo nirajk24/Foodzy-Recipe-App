64ff6c1aafc094890a445f83dcb572d73213cc79
Niraj <kumar.bgp.niraj@gmail.com>
2023-05-25 00:49:00 +0530

    Added icon

---
+++ app/src/main/java/com/example/easyfood/activities/ProfileActivity.kt
@@ -0,0 +1,12 @@
+package com.example.easyfood.activities
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.easyfood.R
+
+class ProfileActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_profile)
+    }
+}
